bf3e1bfd10b30b862265f450d8996772aa3c2f28
3-sha <108625495+3-sha@users.noreply.github.com>
2022-12-27 15:58:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 /* eslint-disable quotes */
 /* eslint-disable semi */
-const app = require("./app");
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("started express server at port ${PORT}");
+const app = require("./app.js");
+const express = require("express");
+const path = require("path");
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("started express server at port 3000");
 });
